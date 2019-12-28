0b523c79cafcf719735d3b48307ebcb4c0510b67
Jonathan Klamroth <jonathan.klamroth@gmail.com>
2019-12-28 19:07:36 +0100

    adjust PID values for Anet A8

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -491,9 +491,9 @@
   //#define DEFAULT_Kd 440
 
   // Anet A8
-  #define DEFAULT_Kp 15.18
-  #define DEFAULT_Ki 0.68
-  #define DEFAULT_Kd 84.27
+  #define DEFAULT_Kp 17.15
+  #define DEFAULT_Ki 0.80
+  #define DEFAULT_Kd 92.04
 
 #endif // PIDTEMP
 
@@ -514,7 +514,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -532,15 +532,20 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
   //#define DEFAULT_bedKp 97.1
   //#define DEFAULT_bedKi 1.41
   //#define DEFAULT_bedKd 1675.16
+
+  // Anet A8
+  #define DEFAULT_bedKp 85.42
+  #define DEFAULT_bedKi 10.86
+  #define DEFAULT_bedKd 447.85
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
